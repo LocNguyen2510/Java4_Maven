ee3e69c75cb1658e811c73855cf85935135fe8ab
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2025-03-31 14:07:28 +0700

    Maven

---
+++ src/main/java/dinhloc/vn/HelloMaven/App.java
@@ -0,0 +1,13 @@
+package dinhloc.vn.HelloMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
